37494c5d8006faca150522a73b0bb3d9941e13f3
Subroto Banerjee <subroto.2003@gmail.com>
2025-03-18 11:37:56 +0530

    list comprehensions

--- pythonrefresh/list_comprehension.py
+++ pythonrefresh/list_comprehension.py
@@ -2,3 +2,12 @@ numbers = [3,5,6]
 doubled = [num * 2 for num in numbers]
 
 print(doubled)
+
+friends = ["Rolf", "Subroto", "Shreya", "Shubham", "Robin"]
+starts_s = [friend for friend in friends if friend.startswith("R")]
+
+# for friend in friends:
+#     if friend.startswith("S"):
+#         starts_s.append(friend)
+
+print(starts_s)
