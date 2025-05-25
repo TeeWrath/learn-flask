9a16c2226872145410ea26f52c28ce13e7fd39a1
Subroto Banerjee <subroto.2003@gmail.com>
2025-05-25 12:44:11 +0530

    decorators with parameters

--- pythonrefresh/decorators.py
+++ pythonrefresh/decorators.py
@@ -1,17 +1,28 @@
+import functools
+
 # role based access control
-user = {"name": "Jonathan", "access_level": "admin"}
+user = {"name": "Jonathan", "access_level": "guest"}
 
-def get_admin_password():
-    print("1234")
-
-def make_secure(func):
-    def secure_function():
-        if(user["access_level"] == "admin"):
-            return func()
-        else:
-            print(f"No admin permission for user {user["name"]}")
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function():
+            if(user["access_level"] == access_level):
+                return func()
+            else:
+                print(f"No {access_level} level permission for user {user["name"]}")
         
-    return secure_function
+        return secure_function
+    return decorator
 
-get_admin_password = make_secure(get_admin_password)
+@make_secure("admin")
+def get_admin_password():
+    print("admin: 1234")
+    
+@make_secure("user")
+def get_dashboard_password():
+    print("user: user password")
+
+# print(get_admin_password.__name__)
 get_admin_password()
+get_dashboard_password()
