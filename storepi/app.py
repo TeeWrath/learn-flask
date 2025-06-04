6472feae0d3a778c78a849d68799a54930486808
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-04 23:28:10 +0530

    delete store and update item

--- storepi/app.py
+++ storepi/app.py
@@ -21,6 +21,15 @@ def create_store():
     stores[store_id] = new_store
     return new_store,201
 
+# delete a store
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message":"Store deleted"},200
+    except KeyError:
+        abort(404,message="Store not found.")
+
 @app.get("/store/<string>:store_id")
 def get_store(store_id):
     try:
@@ -49,20 +58,35 @@ def create_item():
     
     return item,201
 
-@app.get("/item/<string>:item_id")
+@app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
         return {"message" : "item not found"},404
-    
-@app.delete("/item/<string>:item_id")
+
+# delete an item
+@app.delete("/item/<string:item_id>")
 def delete_an_item(item_id):
     try:
         del items[item_id]
         return {"message":"Item deleted"},200
     except KeyError:
         abort(404,message="Item not found.")
+
+# update an item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400,message="Bad request. Ensure 'price' and 'name' are included in JSON payload.")
+    try:
+        item = items[item_id]
+        item |= item_data
+        
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
 
 @app.get("/item")
 def get_all_items():
