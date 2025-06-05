04d0992660a0134f6bbd8fe923719dba6521a431
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-05 20:31:13 +0530

    feat: adding models, tables and db relationships

---
+++ storepi/models/store.py
@@ -0,0 +1,8 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = "stores"
+    
+    id = db.Column(db.Integer,primary_key=True,)
+    name = db.Column(db.String(80),unique=True,nullable=False)
+    items = db.relationship("ItemModel",back_populates="store",lazy="dynamic") # we do lazy dynamic so that it does not fetch all the items pehle se but only after we ask for them
--- storepi/schemas.py
+++ storepi/schemas.py
@@ -1,18 +1,23 @@
 from marshmallow import Schema,fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True) # we did dump only true because id is a property which api cannot receive from user but it is created
     # itself and always only returned to the client. Id will not be used to validate data when incoming request is there
     
     # properties below are marked required hence these will be used to validate the data
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
-    
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)    
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
     
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True,load_only=True)
+    store = fields.Nested(PlainStoreSchema(),dump_only=True)
+    
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
